91e279f02b2fb5c565a4a02e786d28c21f4e8875
Dani <nikiforovdaniel@gmail.com>
2021-07-16 04:55:15 -0300

    Refactoriza el codigo
    Simplifica la lectura del código

--- clase-7-8/js/scripts.js
+++ clase-7-8/js/scripts.js
@@ -1,13 +1,17 @@
+// ######## ELEMENTOS DOM ######### //
+let DOM = {
+    form: document.querySelector('#carta-a-santa'),
+    errores: document.querySelector('#errores'), 
+}
+
+
 // ######## VALIDACION FORMULARIO ######### //
-const $form = document.querySelector('#carta-a-santa');
-$form.onsubmit = validarFormulario;
+DOM.form.onsubmit = validarFormulario;
 
 function validarFormulario (event){
-    const $form = document.querySelector('#carta-a-santa');
-    
-    const nombre = $form.nombre.value;
-    const ciudad = $form.ciudad.value;
-    const descripcionRegalo = $form['descripcion-regalo'].value;
+    const nombre = DOM.form.nombre.value;
+    const ciudad = DOM.form.ciudad.value;
+    const descripcionRegalo = DOM.form['descripcion-regalo'].value;
 
     const errorNombre = validarNombre(nombre);
     const errorCiudad = validarCiudad(ciudad);
@@ -22,18 +26,16 @@ function validarFormulario (event){
     const esExito = manejarErrores(errores) === 0;
     
     if (esExito) {
-      $form.className = 'oculto';
-      document.querySelector('#exito').className = '';
-      setTimeout(function(){
-        window.location.href = 'wishlist.html';
-      }, 5000);
+      redireccionar();
     }
     event.preventDefault();
 }
 
+
+// ######## GESTIÓN DE ERRORES  ######### //
+
 function manejarErrores(errores) {
   const keys = Object.keys(errores);
-  const $errores = document.querySelector('#errores') 
   let cantidadErrores = 0
 
   keys.forEach(key => {
@@ -42,32 +44,45 @@ function manejarErrores(errores) {
     
     if (error && !existeElemento) {
       cantidadErrores++;
-      $form[key].className = "error" 
-      const $error = document.createElement('li')
-      $error.className = 'error__'+[key];
-      $error.innerText = error;
-      $errores.appendChild($error);
+      creaError(key, error);
     }
     else if (!error && existeElemento) {
-      const elementoParaBorrar = document.querySelector('.error__'+[key]);
-      $errores.removeChild(elementoParaBorrar);
-      $form[key].className = "";
+      eliminaError(key);
     }
     else if (error && existeElemento) {
       cantidadErrores++
-      $form[key].className = "error" ;
+      muestraError(key);
     }  
     else {
-      $form[key].className = "";
+      ocultaError(key);
     }
 
   })
   return cantidadErrores;
 
 }
+function creaError (key, error) {
+  DOM.form[key].className = "error" 
+  const $error = document.createElement('li')
+  $error.className = 'error__'+[key];
+  $error.innerText = error;
+  DOM.errores.appendChild($error);
+}
+function eliminaError (key) {
+  const elementoParaBorrar = document.querySelector('.error__'+[key]);
+  DOM.errores.removeChild(elementoParaBorrar);
+  DOM.form[key].className = "";
+}
+function muestraError (key) {
+  DOM.form[key].className = "error" ;
+}
+function ocultaError (key) {
+  DOM.form[key].className = "";
+
+}
 
 
-// ######## VALIDACION INDIVIDUAL ######### //
+// ######## VALIDACIONES INDIVIDUALES ######### //
 
 function validarNombre (nombre) {
     const contieneSoloLetras = /^[a-z]+$/i.test(nombre);
@@ -84,14 +99,12 @@ function validarNombre (nombre) {
       return "";
     }
 }
-
 function validarCiudad (ciudad){
     if (ciudad.length === 0) {
         return ('Campo Ciudad no debe estar vacio')
     }
     return "";   
 }
-    
 function validarDescripcionRegalo(descripcionRegalo) {
     const contieneLetrasYNumeros = /^[a-z0-9]+$/i.test(descripcionRegalo);
     if (descripcionRegalo.length === 0 ) {
@@ -106,4 +119,15 @@ function validarDescripcionRegalo(descripcionRegalo) {
     else {
       return "";
     }
+}
+
+
+// ######## SIGUIENTE PASO SI NO HAY ERRORES ######### //
+
+function redireccionar() {
+  DOM.form.className = 'oculto';
+  document.querySelector('#exito').className = '';
+  setTimeout(function(){
+    window.location.href = 'wishlist.html';
+  }, 5000);
 }
