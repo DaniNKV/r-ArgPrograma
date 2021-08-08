62bf9a826270d39e98b5ee238271171f8efee115
Dani <nikiforovdaniel@gmail.com>
2021-08-08 00:26:23 -0300

    Separando scripts de index y wishlist

--- clase-7-8/js/scripts.js
+++ clase-7-8/js/scripts.js
@@ -145,7 +145,6 @@ function validarDescripcionRegalo(descripcionRegalo) {
 
 
 // ######## SIGUIENTE PASO SI NO HAY ERRORES ######### //
-
 function redireccionar() {
   DOM.form.className = 'oculto';
   document.querySelector('#exito').className = '';
@@ -156,7 +155,6 @@ function redireccionar() {
 
 
 // ######## GUARDAR INFO EN LOCAL STORAGE ######### //
-
 function guardarEnLocalStorage () {
   
   const User = {
@@ -176,89 +174,4 @@ function guardarEnLocalStorage () {
   ));
     
   localStorageIndex++;
-}
-function mostrarDeseos () {
-    let $wishList = document.querySelector('#wish-list');
-    
-    Object
-      .keys(localStorage)
-      .forEach( userKey => {
-        const $li = document.createElement('LI');
-        const data = JSON.parse(localStorage.getItem(userKey))
-        
-        $li.appendChild(document.createTextNode(data.descripcion));
-
-      $wishList.appendChild($li);
-    });
-
-}
-
-
-// ######## ENVIAR REGALO AL USUARIO ######### //
-async function buscarRegalo (regalo) {
-  response = await fetch(`https://api.unsplash.com/photos?per_page=2&query=${regalo}`, {
-    headers: {
-      Authorization: 'Client-ID nFqIGneTClhsupgxyQ1eq5BrZXpyeGGbkl0owLiVS1U'
-    }},
-  )
-  data = await response.json();
-  regaloImage = data[0].urls.regular
-  mostrarRegaloEnDOM(regaloImage)
-}
-
-function llenarContenido () {
-  mostrarComportamientoRandom();
-  mostrarRegalo();
-}
-
-function mostrarComportamientoRandom() {
-  const cantidadComportamientos = Object.keys(comportamiento).length;
-  numeroRandom = CreaNumeroRandom(cantidadComportamientos);
-  
-  const comportamientoRandom = comportamiento[numeroRandom];
-  DOM.actitud.innerText = comportamientoRandom;
-
-}
-
-// Inyectar datos del regalo en el HTML
-
-function mostrarRegalo() {
-  if (DOM.actitud.innerText === 'malo') {
-    DOM.santaGift.innerText = 'No tienes regalo este año :(';
-    const $p = document.querySelector('.gift-section .frase');
-    $p.style.display = 'none';
-
-  }else {
-    const nroRandom = CreaNumeroRandom(localStorageIndex-1);
-    const regalo = JSON.parse(localStorage.getItem('user'+(nroRandom)));
-    const regaloElegido = regalo.descripcion
-  
-    DOM.santaGift.innerText = regaloElegido
-
-    establecerRegalo(regaloElegido);
-  }
-
-}
-
-
-// ######## Funciones Auxiliares ######### //
-function CreaNumeroRandom (limite) {
-  //Crea un numero random entero del 0 al limite
-  numeroRandom = Math.floor(Math.random() * limite) + 1;
-
-  return numeroRandom
-}
-
-function establecerRegalo(regalo) {
-  const regaloElegido = 
-      regalo
-      .trim()
-      .replace(/\s/,'-')
-      .toLowerCase();
-
-  buscarRegalo(regaloElegido);
-}
-
-function mostrarRegaloEnDOM (regalo) {
-  DOM.giftImg.src = regalo
 }
