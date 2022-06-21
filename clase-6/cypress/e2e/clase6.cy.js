2d5a9bc00437c324927662147cde63962e8fd227
daniNKV <nikiforovdaniel@gmail.com>
2022-06-21 02:04:26 -0300

    Test a primera parte

--- clase-6/cypress/e2e/clase6.cy.js
+++ clase-6/cypress/e2e/clase6.cy.js
@@ -1,5 +1,27 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Test', () => {
+  before(() => {
+    cy.visit('http://127.0.0.1:8080')
+  })
+  
+  describe('Ingresa cantidad de familiares', () => {
+    it('Clickea sin ingresar cantidad', () =>{
+      cy.contains('Siguiente Paso').click()
+      cy.get('#erroresPop').should('not.be.empty')
+    })
+    it('Completa el formulario y continua', () => {
+      cy.get('#cantidad-familiares').click().type('2')
+      cy.get('#siguiente-paso').click()
+    })
+  })
+
+  describe('Completa edades', () => {
+    
   })
 })
+/*
+describe('Mi primer test', () => {
+  it('No hace mucho la verdad!', () => {
+    expect(true).to.equal(true)
+  })
+})
+*/
